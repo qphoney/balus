08c33206726e35bf86b9e5a894c2b0d925b3d8b7
qphoney <tateoka@serverworks.co.jp>
2013-08-16 13:09:26 +0900

    シータ王女の装いを変更(sheeta.shに環境変数設定、levistoneの実行を追加)

--- sheeta.sh
+++ sheeta.sh
@@ -1,9 +1,18 @@
-#!/bin/sh
+#!/bin/sh -x
+
+# 私の家に、古い秘密の名前があって、この石を受け継ぐとき、その名前も私継いだの
+. ./levistone
 
 # ルシータ・トエル・ウル・ラピュタ
-export AWS_CREDENTIAL_FILE=./levistone
+#export AWS_CREDENTIAL_FILE=./levistone
+export EC2_REGION=
+export EC2_HOME=
+export PATH=$PATH:$EC2_HOME/bin
+export JAVA_HOME=
+
 
 # AA表示
 jp2a --color ./pics/sheeta1.jpg
 echo "シータ：パズー来ちゃだめ！この人はどうせあたし達を殺す気よ！！"
 sleep 10
+
