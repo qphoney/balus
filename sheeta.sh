f203641a8cae82fdf7013d35252add1576f399c9
qphoney <tateoka@serverworks.co.jp>
2013-08-16 14:54:44 +0900

    シータ姫の装いをまたまた変更(sheeta.shに認証情報、credentialの読み込みを追加)

--- sheeta.sh
+++ sheeta.sh
@@ -1,15 +1,14 @@
-#!/bin/sh -x
+#!/bin/sh
 
 # 私の家に、古い秘密の名前があって、この石を受け継ぐとき、その名前も私継いだの
-. ./levistone
-
 # ルシータ・トエル・ウル・ラピュタ
-#export AWS_CREDENTIAL_FILE=./levistone
 export EC2_REGION=
 export EC2_HOME=
 export PATH=$PATH:$EC2_HOME/bin
 export JAVA_HOME=
-
+export AWS_ACCESS_KEY=
+export AWS_SECRET_KEY=
+export AWS_CREDENTIAL_FILE=./levistone
 
 # AA表示
 jp2a --color ./pics/sheeta1.jpg
